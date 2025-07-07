a5705bbe0e45501bb0c365ea7d4b9739c576f79b
subha-routray <121255482+subha-routray@users.noreply.github.com>
2025-07-07 03:14:44 +0000

    datatypes

---
+++ .devcontainer/01_basic/02_datatypes.js
@@ -0,0 +1,15 @@
+
+"use strict"//terat all js code as newer version
+//alert(3+3) we are using node js
+ //types of datatypes:number
+ //string=>""
+ //biginit
+ //boolean
+ //null=>stand alone value
+ //undefined
+ //symbol
+
+ //obiect:
+ console.log(typeof null)//op:object
+ console.log(typeof undefined)//op:undefined
+
