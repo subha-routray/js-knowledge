9c7299323fc1f254df88725bb9dfda88b00eb4f8
subha-routray <121255482+subha-routray@users.noreply.github.com>
2025-07-07 06:47:26 +0000

    morecontent

--- .devcontainer/01_basic/03_conversionoperation.js
+++ .devcontainer/01_basic/03_conversionoperation.js
@@ -18,3 +18,27 @@ console.log(LoggedInIsBoolean)
 //0=>false
 //""=>false
 //"hi"=>true
+
+
+//***********************operations******************//
+let value=3
+let negvalue=-value
+console.log(negvalue)
+let string1="subh"
+let string2=4
+let string3=string1+string2
+console.log(string3)
+
+
+console.log("1"+2)
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+let gamecounter=100
+gaming=++gamecounter
+console.log(gaming)
+gamecounter++
+console.log(gamecounter)
+
+//mdn postfix and prefix javascript
