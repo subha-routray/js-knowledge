bb12b5ec90d73e94b267a1277ca44dd182547fb8
subha-routray <121255482+subha-routray@users.noreply.github.com>
2025-07-07 03:10:49 +0000

    variables

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,9 @@
+
+const accountid=12345987
+let accountEmail="cjuhu@yahoo.com"
+var accountpassword="12345"
+accountcity="bbsr"
+console.table([accountid,accountEmail,accountpassword,accountcity])
+/*
+prefer not tot use var because issue in block scope and functional scope
+*/
