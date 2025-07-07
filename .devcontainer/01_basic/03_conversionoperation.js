4b71b9e0120660db2cde4a7c13c170fec42126cf
subha-routray <121255482+subha-routray@users.noreply.github.com>
2025-07-07 05:47:36 +0000

    conversionofdatatypes

---
+++ .devcontainer/01_basic/03_conversionoperation.js
@@ -0,0 +1,20 @@
+
+let score=null
+console.log(typeof score)
+console.log((typeof score))
+let valueInNumber=Number(score)
+console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>NAN not anumber
+//"true"=>1
+//"null"=>0
+
+
+let IsLoggerdIn=1
+let LoggedInIsBoolean=Boolean(IsLoggerdIn)
+console.log(LoggedInIsBoolean)
+//1=>1
+//0=>false
+//""=>false
+//"hi"=>true
