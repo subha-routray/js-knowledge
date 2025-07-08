b0acf54e55b3eb49745d78a0496d64d81aa8ff3f
subha-routray <121255482+subha-routray@users.noreply.github.com>
2025-07-08 10:05:18 +0000

    datatypessummary

---
+++ .devcontainer/01_basic/datatypes_summary.js
@@ -0,0 +1,27 @@
+
+//primitive
+//7types:string,number,boolean,null,undefined,symbol(used for making value unique),bigiint
+
+//javascript is 
+
+const score=100
+const scoreval=120.9
+const IsLoggedIn="flase"
+const  tempOut=null
+let UserEmail
+
+const Id=Symbol('908')
+const anotherId=Symbol('908')
+console.log(Id==anotherId)
+
+
+//reference-type or non-primitive
+//include arrays,objects,functions
+const array=["subh","buddy"]
+const MyObj={
+    name:"subh",
+    age:17
+}
+const MyFunction=funvtion(){
+    console.log("hello world")
+}
